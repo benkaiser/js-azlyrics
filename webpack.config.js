a55d57351368bfa4dee3c177f1f84de7a7f20d56
Benjamin Kaiser <benjaminjkaiser@gmail.com>
2018-12-22 21:25:58 -0800

    basic working version

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: ['./src/index.js'],
+  mode: 'production',
+  output: {
+    path: path.join(__dirname, 'out'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env'],
+          plugins: ['@babel/plugin-transform-runtime']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js']
+  }
+};
