ae4e25e5a8b45a5455a5ce854a952265897c69ab
Benjamin Kaiser <benjaminjkaiser@gmail.com>
2018-12-22 21:44:34 -0800

    add library target

--- src/index.js
+++ src/index.js
@@ -39,5 +39,3 @@ export default class Lyrics {
       });
   }
 }
-
-window.AZLyrics = Lyrics;
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
 
 module.exports = {
-  entry: ['./src/index.js'],
+  entry: './src/index.js',
   mode: 'production',
   output: {
     path: path.join(__dirname, 'out'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    library: 'JSAZLyrics'
   },
   module: {
     rules: [
